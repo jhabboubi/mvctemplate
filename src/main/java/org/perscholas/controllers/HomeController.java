00c2d24e31591defa9215d41da37e2cf886f57c6
Jafer Alhaboubi <habboubi@gmail.com>
2021-07-29 12:23:40 -0500

    minor changes

--- src/main/java/org/perscholas/controllers/HomeController.java
+++ src/main/java/org/perscholas/controllers/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -22,7 +23,7 @@ public String home(Model model){
     return "home";
 }
 @RequestMapping(value="/user", method = RequestMethod.POST)
-public String user(@Validated User user, Model model){
+public String user(User user, Model model){
     System.out.println("User " + user.getUserName());
     model.addAttribute("userName", user.getUserName());
     return "userpage";
